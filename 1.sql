9c60a21d74dc565085049b0c73fef490be117945
Ivanushkin Ivan Alexandrovich <80826763+INLAE@users.noreply.github.com>
2023-03-17 17:55:21 +0300

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -1,5 +1,5 @@
-SELECT product_id, name, price, ROW_NUMBER() OVER w as product_number, \
-                                RANK() OVER w as product_rank, \
-                                DENSE_RANK() OVER w as product_dense_rank\
-FROM products\
-WINDOW w AS (ORDER BY price DESC)}
+SELECT product_id, name, price, ROW_NUMBER() OVER w as product_number, 
+                                RANK() OVER w as product_rank, 
+                                DENSE_RANK() OVER w as product_dense_rank
+FROM products
+WINDOW w AS (ORDER BY price DESC)
