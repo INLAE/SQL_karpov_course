357ab598e27eb27bec9ad55d18d6376cbb991627
Ivanushkin Ivan Alexandrovich <80826763+INLAE@users.noreply.github.com>
2023-03-17 21:58:35 +0300

    Add files via upload

---
+++ 5.sql
@@ -0,0 +1,20 @@
+/* отменённые заказы */
+with cancels as (SELECT order_id
+                                        FROM   user_actions
+                                        WHERE  action = 'cancel_order'),diff as (
+/* заказы с разницей во времени */
+SELECT user_id,
+       order_id,
+       time,
+       time - lag(time, 1) OVER (PARTITION BY user_id
+                                 ORDER BY time) as time_diff
+FROM   user_actions
+WHERE  order_id not in (SELECT *
+                        FROM   cancels))
+SELECT user_id,
+       round(extract(epoch
+FROM   avg(time_diff))/3600) as hours_between_orders
+FROM   diff
+WHERE  time_diff is not null
+GROUP BY user_id
+ORDER BY user_id limit 1000
