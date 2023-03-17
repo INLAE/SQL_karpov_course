206e9e7a251fa069da36757767e2a0a185ffcf83
Ivanushkin Ivan Alexandrovich <80826763+INLAE@users.noreply.github.com>
2023-03-17 20:49:29 +0300

    Update 4.sql

--- 4.sql
+++ 4.sql
@@ -1,12 +1,14 @@
 with cancel_orders as (SELECT order_id
-                       FROM   user_actions
-                       WHERE  action = 'cancel_order'
-                       GROUP BY order_id)
+                         FROM   user_actions
+                         WHERE  action = 'cancel_order'
+                         GROUP BY order_id)
 SELECT user_id,
        order_id,
        time,
        row_number() OVER(PARTITION BY user_id
-                         ORDER BY time) as order_number
+                         ORDER BY time) as order_number,
+       lag(time, 1) OVER(PARTITION BY user_id) as time_lag,
+       time - lag(time, 1) OVER(PARTITION BY user_id) as time_diff
 FROM   user_actions
 WHERE  order_id not in (SELECT *
                         FROM   cancel_orders) limit 1000
